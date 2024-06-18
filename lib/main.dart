1532ed798801896a7eae28a6ef518ac69ac6d1e1
Riyam <cs50riyam211@gmail.com>
2024-06-18 01:09:20 -0700

    solving main problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:kadie_app/core/helper_functions/on_generate_route.dart';
-import 'package:kadie_app/features/onBoarding/presentation/views/on_boarding_view.dart';
 import 'package:kadie_app/features/splash/presentation/views/splash_view.dart';
 
 void main() {
